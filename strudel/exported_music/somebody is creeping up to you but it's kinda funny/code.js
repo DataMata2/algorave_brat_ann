6e746bb7810ca287936acbf2f16cc371789f6f74
Baltabay Zhanybek <s2w.agency@gmail.com>
2026-05-03 03:22:38 +0500

    this is some of the first shit i'm putting out

---
+++ strudel/exported_music/somebody is creeping up to you but it's kinda funny/code.js
@@ -0,0 +1,5 @@
+$: n("[1 3 -2 5](7, 8)").scale("<C:minor C:major>").sound("gm_accordion")._punchcard()
+$: sound("[hh hh hh hh*4]")._punchcard()
+$: sound("bd(3, 4)")._punchcard()
+$: sound("hh(5, 8)")._punchcard()
+
