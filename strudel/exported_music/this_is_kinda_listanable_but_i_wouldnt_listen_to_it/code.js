9ecab6a7444884a6ed5def1cdc2a02f4bb2f8158
Baltabay Zhanybek <s2w.agency@gmail.com>
2026-05-03 03:48:55 +0500

    do I have to write the name of the commit each time I'm trying to commit although i'm sort doing the same shit every time?

---
+++ strudel/exported_music/this_is_kinda_listanable_but_i_wouldnt_listen_to_it/code.js
@@ -0,0 +1,9 @@
+$: sound("tr808_oh*8")
+$: sound("bd(4,4)")
+$: sound("cp cp cp cp")
+$: n("[0, 3, 5, 7, 10](3, 5) [2, 5, 7, 0, 9](7, 10)").scale("C:major").sound("wt_digital_curses")
+// $: n("<[[0, 3, 5, 7, 10](2, 8)*4 [2, 5, 7, 0, 9](2, 4)*4] [<[0, 3, 5, 7, 10](3,8) [2, 5, 7, 0, 9](2,8)>*8]>/2").scale("C:minor").sound("wt_digital_curses")
+
+// $: n("[0, 3, 5, 7, 10]/4").scale("C:major").sound("wt_digital_curses")
+// $: n("[-10]/4").scale("C:major").sound("wt_digital_curses")
+// $: n("-6!2 -4!2").scale("C:major").sound("wt_digital_curses")
